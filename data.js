b7c82243cc06b7ce2708c7115e44235b4415065f
riazul islam <riazulislam5280@gmail.com>
2022-11-24 22:39:42 +0600

    project data updated

---
+++ data.js
@@ -0,0 +1,32 @@
+[
+    {
+        id: 1,
+        category: 'fruits',
+        name: 'Apple',
+        price: 160
+    },
+    {
+        id: 2,
+        category: 'fruits',
+        name: 'Orange',
+        price: 140
+    },
+    {
+        id: 3,
+        category: 'fruits',
+        name: 'Orange Green',
+        price: 160
+    },
+    {
+        id: 4,
+        category: 'fruits',
+        name: 'Grape',
+        price: 180
+    },
+    {
+        id: 5,
+        category: 'fruits',
+        name: 'Kiwi',
+        price: 130
+    }
+]
